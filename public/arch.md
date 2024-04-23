93fbe07701270bb20a3951405155f5d767f68291
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-04-23 12:53:46 +0100

    chore: initialize vite, add the os modules and test in watch mode for both client and server

---
+++ public/arch.md
@@ -0,0 +1,1 @@
+The system architecture is x64, the system uptime is: 0 days 2 hours 2324.312 seconds and the string path of the current user's home directory is C:\Users\USER
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,14 @@ import * as os from 'node:os';
 
 const router = express.Router();
 
+function formatTime(seconds) {
+  const days = Math.floor(seconds / (3600 * 24));
+  const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+  const remainingSeconds = seconds % 3600;
+
+  return `${days} days ${hours} hours ${remainingSeconds} seconds`;
+}
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
@@ -19,7 +27,6 @@ router.get('/', function (req, res, next) {
         // process.stdout.write(data);
         writeFile('public/tunde.html', data, err => {
           if (err) throw err;
-          console.log('The file has been saved!');
         });
       });
     })
@@ -34,11 +41,18 @@ router.get('/', function (req, res, next) {
     });
 
   const architecture = os.arch();
-  const data = new Uint8Array(Buffer.from(architecture));
-  writeFile('arch.md', data, (err) => {
-    if (err) throw err;
-    console.log('The os architecture markdown file has been saved!');
-  }); 
+  // const data = new Uint8Array(Buffer.from(architecture));
+  const seconds = os.uptime();
+  const exactTime = formatTime(seconds);
+  const home = os.homedir();
+  try {
+    writeFile('public/arch.md', `The system architecture is ${architecture}, the system uptime is: ${exactTime} and the string path of the current user's home directory is ${home}`, (err) => {
+      if (err) throw err;
+      console.log('The os architecture markdown file has been saved!');
+    });
+  } catch (error) {
+    console.error('An error occurred: ', error);
+  }
 });
 
 export default router;
